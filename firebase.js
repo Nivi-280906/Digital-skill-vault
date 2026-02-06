e9942e1941fdec1af4601046958d7baa1bedf08e
Nivi-280906 <nnivi2535@gmail.com>
2026-02-06 14:37:57 +0530

    Merged local project files

---
+++ firebase.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAAj_buWFwMZb0QwOdebH-FpurUGemoWmg",
+  authDomain: "skillvault-93232.firebaseapp.com",
+  projectId: "skillvault-93232",
+  storageBucket: "skillvault-93232.firebasestorage.app",
+  messagingSenderId: "817089906854",
+  appId: "1:817089906854:web:1abe810f7efbd5b4fc0c67"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const auth = firebase.auth();
